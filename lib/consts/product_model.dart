f29a86d45b0839b44fca835d4d35ddb12e8e644c
Md.Mashiur Rahman <mashuir3@gmail.com>
2024-11-24 01:22:37 +0600

    Product UI

---
+++ lib/consts/product_model.dart
@@ -0,0 +1,117 @@
+import 'package:fooddelivery/consts/images.dart';
+
+class MyProductModel {
+  final String image, name, category;
+  final double price, rate, distance;
+
+  MyProductModel({
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.rate,
+    required this.category,
+    required this.distance,
+  });
+}
+
+List<MyProductModel> myProductModel = [
+  MyProductModel(
+    image: icMisoRamen,
+    name: 'Sapporo Miso',
+    price: 3.9,
+    rate: 5,
+    distance: 150,
+    category: "Ramen",
+  ),
+  MyProductModel(
+    image: 'assets/food-delivery(foodel)/ramen/kurume_ramen.png',
+    name: 'Kurume Ramen',
+    price: 4.3,
+    rate: 4.9,
+    distance: 600,
+    category: "Ramen",
+  ),
+  MyProductModel(
+    image: icKurumeRamen,
+    name: 'Hakata Ramen',
+    price: 3.9,
+    distance: 400,
+    rate: 4.8,
+    category: "Ramen",
+  ),
+  MyProductModel(
+    image: icShrimpRice,
+    name: 'Shrimp Fried Rice',
+    price: 4.9,
+    distance: 800,
+    rate: 4.5,
+    category: "Ramen",
+  ),
+  MyProductModel(
+    image: icFullsetRamen,
+    name: 'Fullset Ramen',
+    price: 5.9,
+    distance: 400,
+    rate: 4.7,
+    category: "Ramen",
+  ),
+  //for burger
+  MyProductModel(
+    image: icGrilledBeefBurger,
+    name: 'Grilled beef burger',
+    price: 33.5,
+    distance: 150,
+    rate: 5.0,
+    category: "Burger",
+  ),
+  MyProductModel(
+    image: icChickenBurger,
+    name: 'Fried Chicken Burger',
+    price: 23.0,
+    distance: 150,
+    rate: 4.7,
+    category: "Burger",
+  ),
+  MyProductModel(
+    image: icCheeseBurger,
+    name: 'Cheese Burger',
+    price: 40.5,
+    distance: 100,
+    rate: 4.5,
+    category: "Burger",
+  ),
+  MyProductModel(
+    image: icBeefBurger,
+    name: 'Beef-Burger',
+    price: 18.5,
+    distance: 200,
+    rate: 5.0,
+    category: "Burger",
+  ),
+  // for salad
+  MyProductModel(
+    image: icVegSalad,
+    name: 'Veg Salad',
+    price: 7.0,
+    distance: 300,
+    rate: 5.0,
+    category: "Salad",
+  ),
+  MyProductModel(
+    image: icMixSalad,
+    name: 'Mix Salad',
+    price: 10.0,
+    distance: 350,
+    rate: 4.8,
+    category: "Salad",
+  ),
+  // for waffle
+  MyProductModel(
+    image: icBerryWaffle,
+    name: 'Berry Bonanza Waffle',
+    price: 10.0,
+    distance: 500,
+    rate: 4.5,
+    category: "Waffle",
+  ),
+];
--- lib/main_screens/home_page.dart
+++ lib/main_screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fooddelivery/consts/category_model.dart';
 import 'package:fooddelivery/consts/colors.dart';
+import 'package:fooddelivery/consts/product_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,211 +19,248 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                children: [
-                  const Expanded(
-                      child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            "Your Location",
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.black54,
-                                fontWeight: FontWeight.w600),
-                          ),
-                          Icon(
-                            Icons.keyboard_arrow_down,
-                            color: kblack,
-                            size: 20,
-                          )
-                        ],
-                      ),
-                      SizedBox(height: 6),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.location_on,
-                            color: korange,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            "Mashiur, Dhaka",
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: kblack,
-                                fontWeight: FontWeight.w600),
-                          )
-                        ],
-                      )
-                    ],
-                  )),
-                  Row(
-                    children: [
-                      Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black12),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: const Icon(
-                            Icons.search,
-                            color: kblack,
-                          )),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Stack(
-                        alignment: AlignmentDirectional.topCenter,
-                        children: [
-                          Container(
-                              padding: const EdgeInsets.all(10),
-                              margin: const EdgeInsets.symmetric(vertical: 15),
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black12),
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: const Icon(
-                                Icons.shopping_cart_outlined,
-                                color: kblack,
-                              )),
-                          Positioned(
-                              right: 0,
-                              top: 0,
-                              child: GestureDetector(
-                                child: Container(
-                                  padding: const EdgeInsets.all(8),
-                                  decoration: const BoxDecoration(
-                                      color: Colors.redAccent,
-                                      shape: BoxShape.circle),
-                                  child: const Text(
-                                    "0",
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 16),
-                                  ),
-                                ),
-                              ))
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
+            headerParts(),
             const SizedBox(height: 35),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "Let's find the best food around you",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35,
-                    letterSpacing: -.4,
-                    color: kblack),
-              ),
-            ),
+            bannerTextParts(),
             const SizedBox(height: 35),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Find by category",
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                        color: kblack),
-                  ),
-                  Text(
-                    "See all",
-                    style: TextStyle(color: korange),
-                  )
-                ],
-              ),
-            ),
+            categoryParts(),
+            const SizedBox(height: 25),
+            categoryListParts(),
             const SizedBox(height: 25),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  // Updated to align left since scroll is added
-                  children: [
-                    ...List.generate(
-                      myCategories.length,
-                      (index) => GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            selectedIndex = index;
-                          });
-                        },
-                        child: Container(
-                          height: 105,
-                          width: 90,
-                          margin: const EdgeInsets.only(right: 10),
-                          // Add spacing between items
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            border: selectedIndex == index
-                                ? Border.all(width: 2.5, color: korange)
-                                : Border.all(color: Colors.white),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Stack(
-                                alignment: AlignmentDirectional.center,
-                                children: [
-                                  Container(
-                                    height: 30,
-                                    width: 47,
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: kblack.withOpacity(0.4),
-                                          offset: const Offset(0, 10),
-                                          blurRadius: 12,
-                                          spreadRadius: 5,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Image.asset(
-                                    myCategories[index].image,
-                                    width: 50,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                myCategories[index].name,
-                                style: const TextStyle(
-                                  color: kblack,
-                                  fontWeight: FontWeight.w600,
+              child: Text(
+                "Result (40)",
+                style: TextStyle(
+                    color: kblack.withOpacity(0.6),
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -.2),
+              ),
+            ),
+            const SizedBox(height: 25),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ...List.generate(
+                      myProductModel.length,
+                      (index) => Padding(
+                          padding: index == 0
+                              ? const EdgeInsets.only(left: 25, right: 25)
+                              : const EdgeInsets.only(right: 25)))
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding categoryListParts() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          // Updated to align left since scroll is added
+          children: [
+            ...List.generate(
+              myCategories.length,
+              (index) => GestureDetector(
+                onTap: () {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+                child: Container(
+                  height: 105,
+                  width: 90,
+                  margin: const EdgeInsets.only(right: 10),
+                  // Add spacing between items
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: selectedIndex == index
+                        ? Border.all(width: 2.5, color: korange)
+                        : Border.all(color: Colors.white),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Stack(
+                        alignment: AlignmentDirectional.center,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 47,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: kblack.withOpacity(0.4),
+                                  offset: const Offset(0, 10),
+                                  blurRadius: 12,
+                                  spreadRadius: 5,
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
+                          Image.asset(
+                            myCategories[index].image,
+                            width: 50,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        myCategories[index].name,
+                        style: const TextStyle(
+                          color: kblack,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding categoryParts() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Find by category",
+            style: TextStyle(
+                fontSize: 22, fontWeight: FontWeight.w600, color: kblack),
+          ),
+          Text(
+            "See all",
+            style: TextStyle(color: korange),
+          )
+        ],
+      ),
+    );
+  }
+
+  Padding bannerTextParts() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Text(
+        "Let's find the best food around you",
+        style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 35,
+            letterSpacing: -.4,
+            color: kblack),
+      ),
+    );
+  }
+
+  Padding headerParts() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        children: [
+          const Expanded(
+              child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Your Location",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Icon(
+                    Icons.keyboard_arrow_down,
+                    color: kblack,
+                    size: 20,
+                  )
+                ],
+              ),
+              SizedBox(height: 6),
+              Row(
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    color: korange,
+                    size: 20,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Mashiur, Dhaka",
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: kblack,
+                        fontWeight: FontWeight.w600),
+                  )
+                ],
+              )
+            ],
+          )),
+          Row(
+            children: [
+              Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black12),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Icon(
+                    Icons.search,
+                    color: kblack,
+                  )),
+              const SizedBox(
+                width: 10,
+              ),
+              Stack(
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.symmetric(vertical: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: const Icon(
+                        Icons.shopping_cart_outlined,
+                        color: kblack,
+                      )),
+                  Positioned(
+                      right: 0,
+                      top: 0,
+                      child: GestureDetector(
+                        child: Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: const BoxDecoration(
+                              color: Colors.redAccent, shape: BoxShape.circle),
+                          child: const Text(
+                            "0",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ))
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
